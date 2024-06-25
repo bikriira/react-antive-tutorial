2382efde625ccefbe3fd47b284e910bf4a4e2cd9
bikriira <bikriiradufasha@gmail.com>
2024-06-25 09:19:25 +0200

    [Learn] react native intro

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableNativeFeedback,
+  Button,
+  Alert,
+  Dimensions,
+} from "react-native";
 
 export default function App() {
+  console.log(Dimensions.get("screen"));
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableNativeFeedback
+        onPress={() => {
+          Alert.alert("My title", "Button tapped", [{ text: "Ok" }]);
+        }}
+      >
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Click Me</Text>
+        </View>
+      </TouchableNativeFeedback>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "dodgerblue",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
   },
 });
